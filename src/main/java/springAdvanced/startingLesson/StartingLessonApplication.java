ba8354305abf9c75c90b3a9972f4f81fc806d359
Safixans <safixongg@gmail.com>
2024-06-14 18:14:14 +0500

    call-external-apis-openFeign are done but some issues with the api url

--- src/main/java/springAdvanced/startingLesson/StartingLessonApplication.java
+++ src/main/java/springAdvanced/startingLesson/StartingLessonApplication.java
@@ -3,32 +3,20 @@ package springAdvanced.startingLesson;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
+
 
 import java.time.Duration;
 
 
 @SpringBootApplication
+@EnableFeignClients
 public class StartingLessonApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StartingLessonApplication.class, args);
     }
 
-    @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
-        return restTemplateBuilder
-                .setConnectTimeout(Duration.ofMillis(3000))
-                .setReadTimeout(Duration.ofMillis(3000))
-                .build();
-
-//        return new RestTemplate();
-    }
-
-    @Bean
-    public WebClient webClient() {
-        return WebClient.create();
-    }
 }
--- src/main/java/springAdvanced/startingLesson/post/PostServiceImpl.java
+++ src/main/java/springAdvanced/startingLesson/post/PostServiceImpl.java
@@ -1,93 +1,3 @@
-/*package springAdvanced.startingLesson.post;
-
-import lombok.NonNull;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-import springAdvanced.startingLesson.post.dtos.CommentCreateDTO;
-import springAdvanced.startingLesson.post.dtos.CommentDTO;
-import springAdvanced.startingLesson.post.dtos.PostCreateDTO;
-import springAdvanced.startingLesson.post.dtos.PostDTO;
-
-import java.util.List;
-
-@Service
-public class PostServiceImpl implements PostService {
-
-    private final PostRepository postRepository;
-
-    public PostServiceImpl(PostRepository postRepository) {
-        this.postRepository = postRepository;
-    }
-
-    @Override
-    public PostDTO getPost(@NonNull Integer id) {
-        Post post = postRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Post is not found"));
-
-        RestTemplate restTemplate = new RestTemplate();
-        String url = "http://localhost:9595/api/comments/{0}/post";
-
-        ResponseEntity<List<CommentDTO>> response = restTemplate.exchange(
-                url,
-                HttpMethod.GET,
-                HttpEntity.EMPTY,
-                new ParameterizedTypeReference<List<CommentDTO>>() {},
-                post.getId()
-        );
-
-        List<CommentDTO> comments = response.getBody();
-
-        return PostDTO.builder()
-                .id(post.getId())
-                .title(post.getTitle())
-                .comments(comments)
-                .body(post.getBody())
-                .build();
-    }
-
-    @Override
-    public PostDTO createPost(@NonNull PostCreateDTO dto) {
-        Post post = Post.builder()
-                .title(dto.getTitle())
-                .body(dto.getBody())
-                .build();
-        postRepository.save(post);
-
-        return PostDTO.builder()
-                .id(post.getId())
-                .title(post.getTitle())
-                .body(post.getBody())
-                .build();
-    }
-    @Override
-    public Void createComment(@NonNull List<CommentCreateDTO> dtos) {
-        RestTemplate restTemplate = new RestTemplate();
-        String url = "http://localhost:9595/api/comments/saveAll";
-
-        HttpEntity<List<CommentCreateDTO>> request = new HttpEntity<>(dtos);
-
-        ResponseEntity<Void> response = restTemplate.postForEntity(url, request, Void.class);
-
-        if (response.getStatusCode().is2xxSuccessful()) {
-            return null;
-        } else {
-            throw new RuntimeException("Failed to create comment");
-        }
-    }*/
-  /*  @Override
-    public Void createComment(@NonNull List<CommentCreateDTO> dtos) {
-        RestTemplate restTemplate = new RestTemplate();
-        String url = "http://localhost:8080/api/posts/comment";
-        restTemplate.postForEntity(url, dtos, Void.class);
-        return null;
-}
-    }*/
-
-
 package springAdvanced.startingLesson.post;
 
 import lombok.NonNull;
@@ -95,7 +5,7 @@ import org.springframework.stereotype.Service;
 import springAdvanced.startingLesson.post.dtos.CommentCreateDTO;
 import springAdvanced.startingLesson.post.dtos.PostCreateDTO;
 import springAdvanced.startingLesson.post.dtos.PostDTO;
-import springAdvanced.startingLesson.resources.CommentResource;
+import springAdvanced.startingLesson.resources.CommentClient;
 
 import java.util.List;
 
@@ -103,10 +13,10 @@ import java.util.List;
 public class PostServiceImpl implements PostService {
 
     private final PostRepository postRepository;
-    private final CommentResource commentResource;
+    private final CommentClient commentResource;
 
 
-    public PostServiceImpl(PostRepository postRepository, CommentResource commentResource) {
+    public PostServiceImpl(PostRepository postRepository, CommentClient commentResource) {
 
 
         this.postRepository = postRepository;
@@ -123,7 +33,7 @@ public class PostServiceImpl implements PostService {
                 .builder()
                 .id(post.getId())
                 .title(post.getTitle())
-                .comments(commentResource.getAllComments(post.getId()))
+                .comments(commentResource.getAllCommentsByID(post.getId()))
                 .body(post.getBody())
                 .build();
     }
@@ -145,6 +55,6 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public void createComment(@NonNull List<CommentCreateDTO> dtos) {
-        commentResource.saveAll(dtos);
+        commentResource.saveAllComments(dtos);
     }
 }
