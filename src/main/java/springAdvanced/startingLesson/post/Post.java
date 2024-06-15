f8d88ec1da8045166a22a42d20382bf53dcee468
Safixans <safixongg@gmail.com>
2024-06-15 19:34:41 +0500

    call-external-apis-TRAVERSON-FOR-working-with-HATEOUS is done successfully these are the code which is written during the tutorial

---
+++ src/main/java/springAdvanced/startingLesson/post/Post.java
@@ -0,0 +1,22 @@
+package springAdvanced.startingLesson.post;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import java.io.Serializable;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Post implements Serializable {
+    private Integer id;
+    private String title;
+    private String body;
+    private Integer userId;
+
+}
