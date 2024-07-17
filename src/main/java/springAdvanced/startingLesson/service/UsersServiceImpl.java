d6f53ad2e3a4394a6f30446a760f75524c9e8744
Safixans <safixongg@gmail.com>
2024-07-17 20:31:03 +0500

    cache-redis is done successfully

--- src/main/java/springAdvanced/startingLesson/service/UsersServiceImpl.java
+++ src/main/java/springAdvanced/startingLesson/service/UsersServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
 import springAdvanced.startingLesson.dtos.PostCreateDTO;
 import springAdvanced.startingLesson.dtos.PostUpdateDTO;
 import springAdvanced.startingLesson.entity.Post;
@@ -47,7 +46,6 @@ public class UsersServiceImpl implements PostService {
     public void delete(Integer id) {
         postRepository.deleteById(id);
 //        cachedPosts.remove(id);
-
     }
 
     @Override
